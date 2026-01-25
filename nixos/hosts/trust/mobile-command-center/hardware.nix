975bb8696e993889f6d94a4459327b3f0965d71e
itsmrvipr <itsmrvipr@viprware.dev>
2026-01-24 22:58:44 -0500

    feature(nixos): wifi fix

--- nixos/hosts/trust/mobile-command-center/hardware.nix
+++ nixos/hosts/trust/mobile-command-center/hardware.nix
@@ -5,4 +5,6 @@
     # This handles CPU and GPU (verify by reading the asus/zephyrus/ga402x/nvidia/default.nix imports which calls shared.nix)
     nixos-hardware.nixosModules.asus-zephyrus-ga402x-nvidia
   ];
+
+  hardware.enableRedistributableFirmware = true;
 }
