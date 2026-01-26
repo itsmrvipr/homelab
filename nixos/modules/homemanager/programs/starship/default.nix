88f88eac6fde835daa6c3fec0648bd0247ad082c
itsmrvipr <itsmrvipr@viprware.dev>
2026-01-26 02:07:16 -0500

    feature(home-manager): chatgpt going ham but if this doesnt work need to revert

--- nixos/modules/homemanager/programs/starship/default.nix
+++ nixos/modules/homemanager/programs/starship/default.nix
@@ -6,7 +6,7 @@
   programs.starship.settings = {
     add_newline = false;
 
-    format = "[┏━](fg:white)$os$directory$git_branch$git_status''${custom.git_spacer}$line_break[┗━](fg:white)$character";
+    format = "[┏━](fg:white)$os$directory$git_branch$git_status\${custom.git_spacer}$line_break[┗━](fg:white)$character";
     right_format = "$cmd_duration$time";
 
     character = {
@@ -14,9 +14,11 @@
       error_symbol   = "[ ](fg:red)";
     };
 
-    custom.git_spacer = {
-      when = "test -z \"$STARSHIP_GIT_BRANCH\"";
-      format = "[](fg:#e6e6e6)";
+    custom = {
+      git_spacer = {
+        when = "test -z \"$STARSHIP_GIT_BRANCH\"";
+        format = "[](fg:#e6e6e6)";
+      };
     };
 
     os = {
