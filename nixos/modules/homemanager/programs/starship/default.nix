caae95399689694421cb7ad145765579b9419739
itsmrvipr <itsmrvipr@viprware.dev>
2026-01-26 01:26:07 -0500

    feature(home-manager): testing some starship formatting 10

--- nixos/modules/homemanager/programs/starship/default.nix
+++ nixos/modules/homemanager/programs/starship/default.nix
@@ -19,7 +19,7 @@
       disabled = false;
       symbols.Linux = "";
       symbols.NixOS = "";
-      format = "[](fg:#3b3b3b)[   ](fg:#e5e5e5 bg:#3b3b3b)[](fg:#3b3b3b bg:#2563eb)";
+      format = "[](fg:#3b3b3b)[   ](fg:#e5e5e5 bg:#3b3b3b)[](fg:#3b3b3b bg:#051650)";
     };
 
     # ---------- DIRECTORY ----------
@@ -27,13 +27,13 @@
       home_symbol = "";
       truncation_length = 0;
       truncate_to_repo = true;
-      format = "[  $path ](fg:#f8fafc bg:#2563eb)";
+      format = "[  ](fg:#f8fafc bg:#051650)[](fg:#051650 bg:#0a2472)[$path](fg:#f8fafc bg:#0a2472)";
     };
 
     # ---------- GIT ----------
     git_branch = {
       symbol = " ";
-      format = "[](fg:#2563eb bg:#16a34a)[ $symbol $branch ](fg:#022c22 bg:#16a34a)";
+      format = "[](fg:#0a2472 bg:#002e00)[ $symbol ](fg:#022c22 bg:#002e00)[](fg:#002e00 bg:#004400)[ $branch ](fg:#022c22 bg:#004400)";
     };
 
     git_status = {
