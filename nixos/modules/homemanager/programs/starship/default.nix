ae4c450d3d67a6e6942c3e9e8bb41a3227d05c75
itsmrvipr <itsmrvipr@viprware.dev>
2026-01-26 00:52:42 -0500

    feature(home-manager): testing some starship formatting 8

--- nixos/modules/homemanager/programs/starship/default.nix
+++ nixos/modules/homemanager/programs/starship/default.nix
@@ -6,8 +6,8 @@
   programs.starship.settings = {
     add_newline = false;
 
-    format = "[┏━](fg:white)$os$directory$git_branch$git_status$line_break[┗━](fg:white)$character";
-    right_format = "($cmd_duration$time";
+    format = "[┏━](fg:white)$os$directory$git_branch$git_status$line_break[┗━](fg:white)";
+    right_format = "$cmd_duration$time";
     
     character = {
       success_symbol = "[┗━](fg:green) ";
@@ -19,7 +19,7 @@
       disabled = false;
       symbols.Linux = "";
       symbols.NixOS = "";
-      format = "[](fg:white)[](fg:black bg:white)[](fg:white bg:blue)";
+      format = "[](fg:#3b3b3b)[](fg:#e5e5e5 bg:#3b3b3b)[](fg:#3b3b3b bg:#2563eb)";
     };
 
     # ---------- DIRECTORY ----------
@@ -27,17 +27,17 @@
       home_symbol = "";
       truncation_length = 0;
       truncate_to_repo = true;
-      format = "[  $path ](fg:white bg:blue)[](fg:blue)";
+      format = "[  $path ](fg:#f8fafc bg:#2563eb)";
     };
 
     # ---------- GIT ----------
     git_branch = {
       symbol = " ";
-      format = "[](fg:blue bg:green)[ $symbol $branch ](fg:black bg:green)";
+      format = "[](fg:#2563eb bg:#16a34a)[ $symbol $branch ](fg:#022c22 bg:#16a34a)";
     };
 
     git_status = {
-      format = "[ $modified$staged$stashed$ahead_behind ](fg:black bg:green)[](fg:green)";
+      format = "[ $modified$staged$stashed$ahead_behind ](fg:#022c22 bg:#22c55e)[](fg:#22c55e)";
       modified = " $count ";
       staged = " $count ";
       stashed = " $count ";
@@ -50,14 +50,14 @@
     cmd_duration = {
       min_time = 0;
       show_milliseconds = true;
-      format = "[](fg:yellow)[  $duration ](fg:black bg:yellow)";
+      format = "[](fg:#f59e0b)[  $duration ](fg:#1f1300 bg:#f59e0b)";
     };
 
     # ---------- TIME ----------
     time = {
       disabled = false;
       time_format = "%H:%M:%S";
-      format = "[](fg:gray)[  $time ](fg:white bg:gray)[](fg:gray)";
+      format = "[](fg:#6b7280)[  $time ](fg:#f9fafb bg:#6b7280)[](fg:#6b7280)";
     };
   };
 }
